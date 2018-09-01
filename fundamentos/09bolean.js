f588083c575783785b3e979196a1e71b43ad88e2
izaiasg <izaias.rogerio96@gmail.com>
2018-09-01 12:34:35 -0300

    01-09-2018

---
+++ fundamentos/09bolean.js
@@ -0,0 +1,11 @@
+let isAtivo = true;
+console.log(isAtivo);
+
+isAtivo = 1;
+console.log(isAtivo);
+
+// Todos os números com exessão do 0 são verdadeiros
+
+console.log(!!3);
+console.log(!!'texto');
+console.log(!!-3);
