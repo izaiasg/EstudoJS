11dda66ba264ced0f3375d47671df6dc25be6903
izaiasg <izaias.rogerio96@gmail.com>
2018-10-11 14:45:59 -0300

    11102018

--- 03funcao/03ParansVariaveis.js
+++ 03funcao/03ParansVariaveis.js
@@ -1,4 +1,19 @@
 /*
     O fato de não receber nenhum parametro não quer diser que não possa passar nenhum
-    
+
+    Mesmo que não se tenha declarado os parametros dá para recebelos atravez do "arguments"
+    Que é um array
 */
+
+function soma() {
+    let soma = 0
+    for (i in arguments){
+        soma += arguments[i]
+    }
+    return soma
+}
+
+console.log(soma())
+console.log(soma(1))
+console.log(soma(1, 3))
+console.log(soma('a', 'b', 'c'))
